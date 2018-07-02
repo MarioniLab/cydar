3346918b3a682119a736785323ececcac51808dd
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-02 14:57:32 +0100

    Updated neighborDistances to use findKNN.

--- R/neighborDistances.R
+++ R/neighborDistances.R
@@ -1,34 +1,19 @@
-neighborDistances <- function(x, neighbors=50, downsample=50, as.tol=TRUE, naive=FALSE) 
+#' @export
+#' @importFrom kmknn findKNN
+neighborDistances <- function(x, neighbors=50, downsample=50, as.tol=TRUE)
 # Calculates the 'tol' required to capture a certain number of neighbors.
 #
 # written by Aaron Lun
 # created 7 July 2016
-# last modified 24 May 2017
 {
-    .check_cell_data(x, check.clusters=!naive)
-    if (naive) {
-        cluster.centers <- cluster.info <- NULL
-    } else {
-        cluster.centers <- metadata(x)$cluster.centers
-        cluster.info <- metadata(x)$cluster.info
-    }
-
-    # Calculating the number of used markers.
-    used <- .chosen_markers(markerData(x)$used, markernames(x))
-    nused <- sum(used)
-
-    # Checking parameters.
-    neighbors <- as.integer(neighbors)
-    if (neighbors <= 0L) { stop("'neighbors' should be a positive integer") }
-    downsample <- as.integer(downsample)
-    if (downsample <= 0L) { stop("'downsample' should be a positive integer") }
+    pre <- .raw_precomputed(x)
+    to.check <- .downsample(x, downsample)
 
     # Computing distances.
-    ci <- .get_used_intensities(x, used)
-    distances <- .Call(cxx_get_nndist, ci, cluster.centers, cluster.info, neighbors, downsample) 
+    distances <- findKNN(precomputed=pre, k=neighbors, get.index=FALSE, subset=pre$order[to.check])$distance
+    distances <- t(distances)
 
     # Converting to tolerance values, if so desired.
-    distances <- t(distances)
     if (as.tol) {
         distances <- distances/sqrt(nused)
     }
