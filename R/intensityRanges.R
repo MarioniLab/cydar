1274a094d8f589acbd05fa61c65296e951b1abc9
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-02 14:18:58 +0100

    Cleaned up intensityRanges.

--- R/intensityRanges.R
+++ R/intensityRanges.R
@@ -1,13 +1,15 @@
+#' @export
+#' @importFrom flowCore markernames
+#' @importFrom stats quantile
 intensityRanges <- function(x, p=0.01)
 # Computes the log-fold change in intensity.
 #
 # written by Aaron Lun
 # created 22 April 2016
-# last modified 21 March 2017
 {
     .check_cell_data(x, check.clusters=FALSE)
-    marker.names <- rownames(markerData(x))
-    ci <- cellIntensities(x)
+    marker.names <- markernames(x)
+    ci <- .raw_cellIntensities(x)
     all.ranges <- vector("list", length(marker.names))
     for (m in seq_along(marker.names)) {
         all.ranges[[m]] <- quantile(ci[m,], p=c(p, 1-p))
