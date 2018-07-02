862033de5c56273fea128cd1bb5f08d08d0ce99e
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-02 14:25:05 +0100

    Updated medIntensities.

--- R/medIntensities.R
+++ R/medIntensities.R
@@ -1,20 +1,20 @@
-medIntensities <- function(x, markers) 
+#' @export
+#' @importFrom flowCore markernames
+#' @importFrom SummarizedExperiment assayNames assay<- assayNames<-
+medIntensities <- function(x)
 # Computes the median intensity for each sample, in each hypersphere,
 # for each marker. The idea is to fit this to a linear model.
 #
 # written by Aaron Lun
 # created 2 December 2016
-# last modified 24 May 2017 
 {
     .check_cell_data(x, check.clusters=FALSE)
-    samples <- colnames(x)
-    used <- .chosen_markers(markers, markernames(x))
-    sample.id <- cellData(x)$sample.id - 1L # Get to zero indexing.
 
-    ci <- .get_used_intensities(x, used)
-    out <- .Call(cxx_compute_median_int, ci, length(samples), sample.id, cellAssignments(x))
+    sample.id <- cellData(x)$sample.id - 1L # Get to zero indexing.
+    ci <- .raw_cellIntensities(x)
+    out <- .Call(cxx_compute_median_int, ci, ncol(x), sample.id, .raw_cellAssignments(x))
     
-    used.markers <- markernames(x)[used]
+    used.markers <- markernames(x)
     old.names <- assayNames(x)
     for (j in seq_along(used.markers)) { 
         assay(x, length(old.names)+j) <- out[[j]]
