7792798d4b4ad6b871d04f825a7e431d6c2e0cad
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-06 11:14:31 +0100

    Reverted findFirstSphere back to old behaviour.

--- R/findFirstSphere.R
+++ R/findFirstSphere.R
@@ -1,4 +1,6 @@
 #' @export
+#' @importFrom kmknn findNeighbors precluster
+#' @importFrom methods is
 findFirstSphere <- function(x, pvalues, threshold=1, block=NULL, naive=FALSE)
 # Returns a logical vector indicating which hyperspheres are redundant
 # within the specified distance threshold.
@@ -7,24 +9,35 @@ findFirstSphere <- function(x, pvalues, threshold=1, block=NULL, naive=FALSE)
 # created 31 October 2016
 {
     if (length(pvalues)!=nrow(x)) {
-        stop("length of 'pvalues' must equal number of rows in 'coords'")
+        stop("length of 'pvalues' must equal number of rows in 'x'")
+    }
+
+    if (is(x, "CyData")) {
+        .check_cell_data(x)
+        x <- .raw_intensities(x)
     }
 
     if (!is.null(block)) {
         # Identifying unique elements within each block.
-        if (length(block)!=nrow(coords)) {
-            stop("length of 'block' must equal number of rows in 'coords'")
+        if (length(block)!=nrow(x)) {
+            stop("length of 'block' must equal number of rows in 'x'")
         }
         by.block <- split(seq_along(block), block)
         total.out <- logical(length(block))
         for (b in by.block) {
-            total.out[b] <- Recall(coords[b,,drop=FALSE], pvalues[b], threshold=threshold, block=NULL, naive=naive)
+            total.out[b] <- Recall(x[b,,drop=FALSE], pvalues[b], threshold=threshold, block=NULL, naive=naive)
         }
         return(total.out)
     }
 
-    # Checking for non-redundancy.
-    .check_cell_data(x)
-    .Call(cxx_drop_redundant, order(pvalues) - 1L, .raw_center_cell(x), cellAssignments(x))
+    # Using findNeighbors to screen out candidates based on the enclosing hypersphere.
+    pre <- precluster(x)
+    MULT <- max(1, sqrt(nrow(x)))
+    potential <- findNeighbors(precomputed=pre, threshold=threshold * MULT, get.distance=FALSE, raw.index=TRUE)$index
+
+    pvalues <- pvalues[pre$order]
+    out <- .Call(cxx_drop_redundant, pre$data, order(pvalues) - 1L, potential, threshold)
+    out[pre$order] <- out
+    return(out)
 }
 
--- src/drop_redundant.cpp
+++ src/drop_redundant.cpp
@@ -1,34 +1,51 @@
 #include "cydar.h"
+#include "utils.h"
 
-SEXP drop_redundant (SEXP ordering, SEXP center_id, SEXP assignments) {
+SEXP drop_redundant (SEXP intensities, SEXP ordering, SEXP assignments, SEXP threshold) {
     BEGIN_RCPP
 
     const Rcpp::List Assignments(assignments);
     const int ngroups=Assignments.size();
-
-    const Rcpp::IntegerVector center_cell(center_id);
-    if (center_cell.size()!=ngroups) {
-        throw std::runtime_error("length of 'center_id' is not equal to the number of groups");
-    }
 
     const Rcpp::IntegerVector Ordering(ordering);
     if (Ordering.size()!=ngroups) {
         throw std::runtime_error("length of 'ordering' is not equal to the number of groups");
     }
 
-    // Looking for hypersphers that are not redundant to hyperspheres with lower p-values.
+    const Rcpp::NumericMatrix Intensities(intensities);
+    if (ngroups!=Intensities.ncol()) {
+        throw std::runtime_error("length of 'ordering' is not equal to number of columns in 'intensities'");
+    }
+    const int nmarkers=Intensities.nrow();
+
+    const double Threshold=check_numeric_scalar(threshold, "threshold");
+
+    // Looking for points that are not redundant to points with lower p-values.
     Rcpp::LogicalVector output(ngroups);
     std::deque<bool> already_seen(ngroups, false);
 
     for (auto o : Ordering) {
-        if (already_seen[center_cell[o]-1]) { 
+        if (already_seen[o]) { 
             continue; 
         }
-        output[o]=1; // yes, 'o' is deliberate here; we're referring to the index of 'assignments'.
+        output[o]=1; 
+        auto curint=Intensities.column(o);
 
         const Rcpp::IntegerVector neighbors=Assignments[o];
-        for (const auto& neigh : neighbors) { 
-            already_seen[neigh-1] = true;
+        for (const auto& neigh : neighbors) {
+            auto neighint=Intensities.column(neigh - 1);
+            bool is_within=true;
+
+            for (int m=0; m<nmarkers; ++m) {
+                if (std::abs(neighint[m] - curint[m]) > Threshold) {
+                    is_within=false;
+                    break;
+                }
+            }
+
+            if (is_within) {
+                already_seen[neigh-1] = true;
+            }
         }
     }
 
