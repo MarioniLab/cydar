0f8d3602e6537b9a7b3af1f01a330d4fe024f0ab
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-07 12:06:18 +0100

    Cleaned up downsampling utility function.

--- R/utils.R
+++ R/utils.R
@@ -43,17 +43,8 @@
 } 
 
 .downsample <- function(x, downsample) 
-# This determines which points should be used upon downsampling.
-# There are three levels of indices that we need to consider:
-#   i) the ordering of cells within each batch as originally supplied, stored as 'cell.id'.
-#  ii) the ordering of cells in the merged matrix but _before_ reordering, stored as 'precomputed$order'.
-# iii) the actual ordering of cells in the columns of the expression matrix returned by 'precluster()'.
-# 
-# Downsampling is done based on every 'nth' cell, using the ordering in (i).
-# Note that 'cell.id' is already permuted to reflect the reordered matrix in (iii).
-# Thus, 'i' refers to the columns of the reordered matrix that we would like to use for neighbour searching.
-# However, findKNN() and friends require the indices of the matrix before reordering; hence the use of (ii).
+# Find points should be used upon downsampling, based on original cell IDs.
+# This ensures that the same points are chosen regardless of the samples used.
 {
-    i <- ((.raw_cell_id(x) - 1L) %% as.integer(downsample))==0L
-    which(i)
+    which(.raw_cell_id(x) %% as.integer(downsample) == 1L)
 }
