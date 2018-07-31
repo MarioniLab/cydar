7a30a7de8a70c4a5db6cbc4633f68c8bbcbcf5c4
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-31 11:04:35 +0100

    Bugfix to createColorBar.

--- R/createColorBar.R
+++ R/createColorBar.R
@@ -14,7 +14,7 @@ createColorBar <- function(colors, top.text=NULL, bottom.text=NULL, lower=-0.5, 
         top.text <- tail(names(colors), 1L)
     } 
     if (!is.null(top.text)) {
-        text(x.pos, lower, pos=1, bottom.text, cex=cex)
+        text(x.pos, lower, pos=1, top.text, cex=cex)
     }
 
     if (is.null(bottom.text)) {
