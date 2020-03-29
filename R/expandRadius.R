d493e59d48af4cbe7ccd86a765efb86c70a9121a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-03-29 03:53:05 -0700

    Migrated docs for more functions to roxygen.

--- R/expandRadius.R
+++ R/expandRadius.R
@@ -1,17 +1,61 @@
+#' Expand the hypersphere radius
+#' 
+#' Expands the hypersphere radius to account for intensity shifting between non-barcoded samples.
+#' 
+#' @param prepared A \linkS4class{List} object containing a \linkS4class{BiocNeighborIndex} object,
+#' typically produced by \code{\link{prepareCellData}}.
+#' @param design A numeric matrix specifying the experimental design.
+#' @param tol A numeric scalar proportional to the hypersphere radius, see \code{\link{countCells}}.
+#' 
+#' @details
+#' This function increases the hypersphere radius to account for random shifts in marker intensity between non-barcoded samples.
+#' The required increase is estimated by taking the mean of all intensities for each marker in each sample;
+#' computing the variance of the mean intensities across samples for each marker;
+#' and taking the mean variance across all markers.
+#' This is equivalent to the square of the extra distance between cells caused by intensity shifts between samples.
+#' 
+#' The estimated increase is added onto \code{tol}, and the returned value can be directly used in the \code{tol} argument of \code{\link{countCells}}.
+#' This expands the hyperspheres to ensure that corresponding subpopulations in different samples are still counted together.
+#' Otherwise, an intensity shift in one sample may move the cells in a subpopulation out of a hypersphere.
+#' This will inflate the variability if it occurs between replicate samples, and introduce spurious differences if it occurs between samples in different conditions.
+#' 
+#' @return
+#' A numeric scalar specifying a modified \code{tol} to use in \code{\link{countCells}}.
+#' 
+#' @examples
+#' ### Mocking up some data: ###
+#' nmarkers <- 20
+#' marker.names <- paste0("X", seq_len(nmarkers))
+#' nsamples <- 8
+#' sample.names <- paste0("Y", seq_len(nsamples))
+#' 
+#' x <- list()
+#' for (i in sample.names) {
+#'     ex <- matrix(rgamma(nmarkers*1000, 2, 2), ncol=nmarkers, nrow=1000)
+#'     ex <- t(t(ex) + rnorm(nmarkers, 0, 0.25)) # Adding a shift per marker
+#'     colnames(ex) <- marker.names
+#'     x[[i]] <- ex
+#' }
+#' 
+#' ### Running the function: ###
+#' cd <- prepareCellData(x)
+#' expandRadius(cd)
+#' 
+#' @author
+#' Aaron Lun
+#' 
+#' @seealso
+#' \code{\link{prepareCellData}}, to generate the required input.
+#'
+#' \code{\link{countCells}}, where the \code{tol} can be set to the output of this function.
+#' 
 #' @export
 #' @importFrom stats lm.fit
 #' @importFrom BiocNeighbors bndata
-expandRadius <- function(x, design=NULL, tol=0.5) 
-# This computes the standard deviation in the mean intensities,
-# in order to compute the radius expansion required for handling
-# randomly-distributed intensity shifts between samples.
-#
-# written by Aaron Lun
-# created 27 October 2016   
-{
-    ci <- bndata(x$precomputed)
-    sample.id <- x$sample.id
-    nsamples <- nrow(x$colData)
+expandRadius <- function(prepared, design=NULL, tol=0.5) {
+    ci <- bndata(prepared$precomputed)
+    sample.id <- prepared$sample.id
+    nsamples <- nrow(prepared$colData)
 
     # Computing mean intensities for all (used) markers in all samples.
     all.means <- vector("list", nsamples)
@@ -32,5 +76,5 @@ expandRadius <- function(x, design=NULL, tol=0.5)
     # Adding to default tolerance to get per-marker radius.
     # This is done based on squared distance, hence the squaring and rooting.
     # Doubling the shift to account for distances between cells, not just distance from cell to mean.
-    return(sqrt(2*shift2 + tol^2))
+    sqrt(2*shift2 + tol^2)
 }
--- R/neighborDistances.R
+++ R/neighborDistances.R
@@ -1,13 +1,78 @@
+#' Compute distances to neighbors
+#'
+#' Calculate the distances in high-dimensional space to the neighboring cells.
+#' 
+#' @param prepared A \linkS4class{List} object containing a \linkS4class{BiocNeighborIndex} object,
+#' typically produced by \code{\link{prepareCellData}}.
+#' @param neighbors An integer scalar specifying the number of neighbours.
+#' @param downsample An integer scalar specifying the frequency with which cells are examined.
+#' @param as.tol A logical scalar specifying if the distances should be reported as tolerance values.
+#' 
+#' @details
+#' This function examines each cell at the specified downsampling frequency, and computes the Euclidean distances to its nearest neighbors.
+#' If \code{as.tol=TRUE}, these distances are reported on the same scale as \code{tol} in \code{\link{countCells}}.
+#' This allows users to choose a value for \code{tol} based on the output of this function.
+#' Otherwise, the distances are reported without modification.
+#' 
+#' To visualize the distances/tolerances, one option is to use boxplots, as shown below.
+#' Each boxplot represents the distribution of tolerances required for hyperspheres to contain a certain number of cells.
+#' For example, assume that at least 20 cells in each hypersphere are needed to have sufficient power for hypothesis testing.
+#' Now, consider all hyperspheres that are large enough to include the 19th nearest neighbour.
+#' The average distance required to do so would be the median of the boxplot generated from the 19th column of the output.
+#' 
+#' % It's fair to say that most hyperspheres should contain around 20 cells.
+#' % If we use the median distance to the 19th nearest neighbor, then - well - exactly 50% of hyperspheres would be above 20, and 50% would be below 20.
+#' 
+#' Another option is to examine the distribution of counts at a given tolerance/distance.
+#' This is done by counting the number of hyperspheres with a particular number of nearest neighbors closer than the specified tolerance.
+#' In this manner, the expected count distribution from setting a particular tolerance can be determined.
+#' Note that the histogram is capped at \code{neighbors} to save time.
+#' 
+#' Note that, for each examined cell, its neighbors are identified from the full set of cells.
+#' Downsampling only changes the rate at which cells are examined, for the sake of computational efficiency.
+#' Neighbors are not identified from the downsampled set as this will inflate the reported distances.
+#' 
+#' @return
+#' A numeric matrix of distances where each row corresponds to an examined cell and each column \code{i} corresponds to the \code{i}th closest neighbor.
+#' 
+#' @examples
+#' example(prepareCellData, echo=FALSE)
+#' 
+#' distances <- neighborDistances(cd, as.tol=FALSE)
+#' boxplot(distances, xlab="Neighbor", ylab="Distance")
+#' 
+#' # Making a plot to choose 'tol' in countCells().
+#' distances <- neighborDistances(cd, as.tol=TRUE)
+#' boxplot(distances, xlab="Neighbor", ylab="Tolerance")
+#' 
+#' required.count <- 20 # 20 cells per hypersphere 
+#' med <- median(distances[,required.count-1]) 
+#' segments(-10, med, required.count-1, col="dodgerblue")
+#' segments(required.count-1, med, y1=0, col="dodgerblue")
+#' 
+#' # Examining the distribution of counts at a given 'tol' of 0.7.
+#' # (Adding 1 to account for the cell at the centre of the hypersphere.)
+#' counts <- rowSums(distances <= 0.7) + 1
+#' hist(counts, xlab="Count per hypersphere")
+#' 
+#' @author
+#' Aaron Lun
+#' 
+#' @seealso
+#' \code{\link{prepareCellData}}, to generate the \code{prepared} object.
+#'
+#' \code{\link{countCells}}, where the choice of \code{tol} can be guided by the distance distributions.
+#'
 #' @export
 #' @importFrom BiocNeighbors findKNN
-neighborDistances <- function(x, neighbors=50, downsample=50, as.tol=TRUE)
+neighborDistances <- function(prepared, neighbors=50, downsample=50, as.tol=TRUE)
 # Calculates the 'tol' required to capture a certain number of neighbors.
 #
 # written by Aaron Lun
 # created 7 July 2016
 {
-    pre <- x$precomputed
-    to.check <- .downsample0(x$cell.id, downsample)
+    pre <- prepared$precomputed
+    to.check <- .downsample0(prepared$cell.id, downsample)
 
     # Computing distances.
     distances <- findKNN(BNINDEX=pre, k=neighbors, get.index=FALSE, subset=to.check, raw.index=TRUE)$distance
